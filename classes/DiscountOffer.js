01fef3c037085fb290aea6161d9b4baf6a081241
abarreaux <alexis.barreaux@gmail.com>
2023-07-29 16:30:46 +0200

    Making DiscountOffer an interface

---
+++ classes/DiscountOffer.js
@@ -0,0 +1,22 @@
+import { BackMarketDiscountOffer } from "./BackMarketDiscountOffer";
+import { IlekDiscountOffer } from "./IlekDiscountOffer";
+import { NaturaliaDiscountOffer } from "./NaturaliaDiscountOffer";
+import { VintedDiscountOffer } from "./VintedDiscountOffer";
+import { BaseDiscountOffer } from "./BaseDiscountOffer";
+
+export class DiscountOffer {
+    constructor(partnerName, expiresIn, discountRateInPercent) {
+        switch (partnerName) {
+            case "BackMarket":
+                return new BackMarketDiscountOffer(expiresIn, discountRateInPercent);
+            case "Ilek":
+                return new IlekDiscountOffer(expiresIn, discountRateInPercent);
+            case "Naturalia":
+                return new NaturaliaDiscountOffer(expiresIn, discountRateInPercent);
+            case "Vinted":
+                return new VintedDiscountOffer(expiresIn, discountRateInPercent);
+            default:
+                return new BaseDiscountOffer(expiresIn, discountRateInPercent);
+        }
+    }
+}
