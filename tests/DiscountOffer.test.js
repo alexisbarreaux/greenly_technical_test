d4fe10e1950902a9275035e75f82d4eb27829fd2
abarreaux <alexis.barreaux@gmail.com>
2023-07-29 19:16:44 +0200

    Adding tests for DiscountOfferInterface

--- tests/DiscountOffer.test.js
+++ tests/DiscountOffer.test.js
@@ -1,57 +1,29 @@
 import { DiscountOffer } from "../classes/DiscountOffer";
+import { BackMarketDiscountOffer } from "../classes/BackMarketDiscountOffer";
+import { BaseDiscountOffer } from "../classes/BaseDiscountOffer";
+import { IlekDiscountOffer } from "../classes/IlekDiscountOffer";
+import { NaturaliaDiscountOffer } from "../classes/NaturaliaDiscountOffer";
+import { VintedDiscountOffer } from "../classes/VintedDiscountOffer";
 
 describe("DiscountOffer", () => {
-    describe("expiresIn", () => {
-        it("should decrease by 1 when valid", () => {
-            const discountOffer = new DiscountOffer("test", 2, 3)
-            discountOffer.updateDiscount()
-            expect(discountOffer.expiresIn).toEqual(
-                1
-            );
-        });
-        it("should decrease  by 1 when expired", () => {
-            const discountOffer = new DiscountOffer("test", 0, 3)
-            discountOffer.updateDiscount()
-            expect(discountOffer.expiresIn).toEqual(
-                -1
-            );
-        });
+    it("should build BackMarketDiscountOffer when provided BackMarket as partnerName", () => {
+        expect(new DiscountOffer("BackMarket", 2, 3)).toEqual(new BackMarketDiscountOffer(2, 3)
+        );
     });
-    describe("discountInPercent", () => {
-        it("should decrease by 1 when valid", () => {
-            const discountOffer = new DiscountOffer("test", 2, 7)
-            discountOffer.updateDiscount()
-            expect(discountOffer.discountInPercent).toEqual(
-                6
-            );
-        });
-        it("should decrease by two 2 when expired", () => {
-            const discountOffer = new DiscountOffer("test", 0, 7)
-            discountOffer.updateDiscount()
-            expect(discountOffer.discountInPercent).toEqual(
-                5
-            );
-        });
-        it("should decrease by 1 when expiring today", () => {
-            const discountOffer = new DiscountOffer("test", 1, 7)
-            discountOffer.updateDiscount()
-            expect(discountOffer.discountInPercent).toEqual(
-                6
-            );
-        });
-        it("should not decrease below 0", () => {
-            const discountOffer = new DiscountOffer("test", 2, 0)
-            discountOffer.updateDiscount()
-            expect(discountOffer.discountInPercent).toEqual(
-                0
-            );
-        });
-        it("should not decrease below 0 when decreasing by 2", () => {
-            const discountOffer = new DiscountOffer("test", 0, 1)
-            discountOffer.updateDiscount()
-            expect(discountOffer.discountInPercent).toEqual(
-                0
-            );
-        });
+    it("should build BaseDiscountOffer when provided random partnerName", () => {
+        expect(new DiscountOffer("test", 2, 3)).toEqual(new BaseDiscountOffer("test", 2, 3)
+        );
+    });
+    it("should build IlekDiscountOffer when provided Ilek as partnerName", () => {
+        expect(new DiscountOffer("Ilek", 2, 3)).toEqual(new IlekDiscountOffer(2, 3)
+        );
+    });
+    it("should build NaturaliaDiscountOffer when provided Naturalia as partnerName", () => {
+        expect(new DiscountOffer("Naturalia", 2, 3)).toEqual(new NaturaliaDiscountOffer(2, 3)
+        );
+    });
+    it("should build VintedDiscountOffer when provided Vinted as partnerName", () => {
+        expect(new DiscountOffer("Vinted", 2, 3)).toEqual(new VintedDiscountOffer(2, 3)
+        );
     });
 });
